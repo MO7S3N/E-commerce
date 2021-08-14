710211b59e63b1cedb8f46b098fb28774250d4e1
MO7S3N <rider1@outlook.fr>
2021-08-14 19:38:39 +0100

    intial commit

---
+++ src/Controller/BackController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BackController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="back")
+     */
+    public function index(): Response
+    {
+        return $this->render('back/index.html.twig', [
+        ]);
+    }
+}
